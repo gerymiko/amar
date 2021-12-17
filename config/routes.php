0229355ed70de3db0cdd823817d536e6b995b319
ylzzirg <gery.merdiko@gmail.com>
2021-12-17 17:26:27 +0700

    clear

--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Slim\App;
-
-return function (App $app) {
-    $app->get('/', \App\Controller\HomeController::class)->setName('home');
-
-    $app->post('/submission', \App\Controller\SubmissionController::class);
-};
